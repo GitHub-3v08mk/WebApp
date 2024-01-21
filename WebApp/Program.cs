6aae5d2153a75ff3b0b813b9ad85146a601cd8e7
Arne Decker <arne.decker1@outlook.com>
2024-01-21 21:24:04 +0000

    Changed to HTTP

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -3,13 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 app.UseExceptionHandler("/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
-app.UseRouting();
+//app.UseHsts();
 //app.UseHttpsRedirection();
-//app.UseRouting();
+app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFiles();
